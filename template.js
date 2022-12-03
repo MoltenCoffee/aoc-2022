01e303b908ed9f1e1d4a3eda2a3783b92f68bc30
MoltenCoffee <13321277+MoltenCoffee@users.noreply.github.com>
2022-12-03 12:09:52 +0100

    Template

---
+++ template.js
@@ -0,0 +1,12 @@
+import openInput from "./helpers/openInput.js";
+
+(async () => {
+  const data = (await openInput(process.argv[2])).split("\n");
+
+  const part1 = null;
+
+  const part2 = null;
+
+  console.log("Part 1", part1);
+  console.log("Part 2", part2);
+})();
